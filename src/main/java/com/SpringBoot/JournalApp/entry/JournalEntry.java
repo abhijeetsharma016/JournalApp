9f4a32387147c78cc88f79ef2ad2e5be49540ba1
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2025-06-08 08:42:05 +0530

    added user and lombook

--- src/main/java/com/SpringBoot/JournalApp/entry/JournalEntry.java
+++ src/main/java/com/SpringBoot/JournalApp/entry/JournalEntry.java
@@ -1,53 +1,21 @@
 package com.SpringBoot.JournalApp.entry;
 
+import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.Date;
 
 @Document(collection = "journal_entries")
+@Data
+@NoArgsConstructor
 public class JournalEntry {
-
     @Id
     private ObjectId id;
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
+    @NonNull
     private String title;
     private String content;
     private LocalDateTime date;
-
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-
-  public ObjectId get_id(){
-      return id;
-  }
-
-  public void setId(ObjectId id){
-      this.id = id;
-  }
-
-  public String getTitle(){
-      return title;
-  }
 }
