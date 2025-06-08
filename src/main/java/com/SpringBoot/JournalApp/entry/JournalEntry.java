4128385aa56dcf72eb5958d1b0751179ef735c6a
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2025-06-08 07:53:37 +0530

    created user

--- src/main/java/com/SpringBoot/JournalApp/entry/JournalEntry.java
+++ src/main/java/com/SpringBoot/JournalApp/entry/JournalEntry.java
@@ -1,5 +1,8 @@
 package com.SpringBoot.JournalApp.entry;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,46 +11,17 @@ import java.time.LocalDateTime;
 import java.util.*;
 
 @Document(collection = "journal_entries")
+@Data //getters setters and everything
 public class JournalEntry {
 
     @Id
     private ObjectId id;
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 
     private String title;
     private String content;
     private LocalDateTime date;
 
 
-    public LocalDateTime getDate() {
-        return date;
-    }
 
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-
-  public ObjectId get_id(){
-      return id;
-  }
-
-  public void setId(ObjectId id){
-      this.id = id;
-  }
-
-  public String getTitle(){
-      return title;
-  }
 }
