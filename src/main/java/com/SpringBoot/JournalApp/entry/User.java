8372b5524a5336ec8ac61c98ffbd4e0cc4f26520
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2025-07-16 00:43:00 +0530

    updated user entry

--- src/main/java/com/SpringBoot/JournalApp/entry/User.java
+++ src/main/java/com/SpringBoot/JournalApp/entry/User.java
@@ -1,74 +1,31 @@
 package com.SpringBoot.JournalApp.entry;
 
+import com.mongodb.lang.NonNull;
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.index.Indexed;
 
 import java.util.ArrayList;
 import java.util.List;
-
 @Document(collection = "users")
+@Data
 public class User {
 
     @Id
     private ObjectId id;
 
     @Indexed(unique = true)
+    @NonNull
     private String userName;
 
+    @NonNull
     private String password;
 
+    @DBRef
     private List<JournalEntry> journalEntries = new ArrayList<>();
 
-    // Constructors
-    public User() {}
-
-    public User(String userName, String password) {
-        this.userName = userName;
-        this.password = password;
-    }
-
-    // Getters and Setters
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public List<JournalEntry> getJournalEntries() {
-        return journalEntries;
-    }
-
-    public void setJournalEntries(List<JournalEntry> journalEntries) {
-        this.journalEntries = journalEntries;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", userName='" + userName + '\'' +
-                ", password='" + password + '\'' +
-                ", journalEntries=" + journalEntries +
-                '}';
-    }
+    private List<String> roles;
 }
