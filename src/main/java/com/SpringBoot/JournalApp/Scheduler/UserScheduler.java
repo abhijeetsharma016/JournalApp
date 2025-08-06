d0389f5b6c0ba91d88123d34a1030fbbe383f8f2
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2025-08-07 03:37:12 +0530

    now i can send sentiment via mail

--- src/main/java/com/SpringBoot/JournalApp/Scheduler/UserScheduler.java
+++ src/main/java/com/SpringBoot/JournalApp/Scheduler/UserScheduler.java
@@ -2,21 +2,24 @@
 package com.SpringBoot.JournalApp.Scheduler;
 
 
-        import com.SpringBoot.JournalApp.Service.EmailService;
-        import com.SpringBoot.JournalApp.Service.SentimentAnalysisService;
-        import com.SpringBoot.JournalApp.entry.JournalEntry;
-        import com.SpringBoot.JournalApp.entry.User;
-        import com.SpringBoot.JournalApp.repositor.UserRepository;
-        import com.SpringBoot.JournalApp.repositor.UserRepositoryImpl;
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.scheduling.annotation.Scheduled;
-        import org.springframework.stereotype.Component;
+import com.SpringBoot.JournalApp.Service.EmailService;
+import com.SpringBoot.JournalApp.cache.AppCache;
+import com.SpringBoot.JournalApp.entry.JournalEntry;
+import com.SpringBoot.JournalApp.entry.User;
+import com.SpringBoot.JournalApp.enums.Sentiment;
+import com.SpringBoot.JournalApp.repositor.UserRepository;
+import com.SpringBoot.JournalApp.repositor.UserRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
-        import java.time.LocalDate;
-        import java.time.LocalDateTime;
-        import java.time.temporal.ChronoUnit;
-        import java.util.List;
-        import java.util.stream.Collectors;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 public class UserScheduler {
@@ -29,24 +32,46 @@ public class UserScheduler {
     private UserRepositoryImpl userRepository;
 
     @Autowired
-    private SentimentAnalysisService sentimentAnalysisService;
+    private AppCache appCache;
 
 
-    @Scheduled(cron = "* * * ? * *") // Every 30 seconds for testing
+    // @Scheduled(cron = "0 0 9 * * SUN")
     public void fetchUsersAndSendSaMail() {
-        System.out.println("Scheduler triggered!"); // Add this line
         List<User> users = userRepository.getUserForSA();
-        System.out.println("Found users: " + users.size()); // Add this line
 
         for (User user : users) {
+            // Filter journal entries from the last 7 days
             List<JournalEntry> journalEntries = user.getJournalEntries();
+            List<Sentiment> sentiments = journalEntries.stream()
+                    .filter(x -> x.getDate().isAfter(LocalDateTime.now().minus(7, ChronoUnit.DAYS)))
+                    .map(x -> x.getSentiment())
+                    .collect(Collectors.toList());
 
-            List<String> filteredEntries = journalEntries.stream().filter(x -> x.getDate().isAfter(LocalDateTime.now().minus(7, ChronoUnit.DAYS))).map(x -> x.getContent()).collect(Collectors.toList()).reversed();
 
+            // Count frequency of each sentiment
+            Map<Sentiment, Integer> sentimentCounts = new HashMap<>();
+            for (Sentiment sentiment : sentiments) {
+                if (sentiment != null) {
+                    sentimentCounts.put(sentiment, sentimentCounts.getOrDefault(sentiment, 0) + 1);
+                }
+            }
 
-            String entry = String.join(" ", filteredEntries);
-            String sentiment = sentimentAnalysisService.getSentiment(entry);
-            //emailService.sendEmail(user.getEmail(), "Sentiment for last 7 days", sentiment);
+            // Find the most frequent sentiment
+            Sentiment mostFrequentSentiment = null;
+            int maxCount = 0;
+            for (Map.Entry<Sentiment, Integer> entry : sentimentCounts.entrySet()) {
+                if (entry.getValue() > maxCount) {
+                    maxCount = entry.getValue();
+                    mostFrequentSentiment = entry.getKey();
+                }
+            }
+
+            // Send email if sentiment found
+            if (mostFrequentSentiment != null) {
+                emailService.sendEmail(user.getEmail(),
+                        "Sentiment for last 7 days",
+                        mostFrequentSentiment.toString());
+            }
         }
     }
 }
---
+++ src/main/java/com/SpringBoot/JournalApp/enums/Sentiment.java
@@ -0,0 +1,8 @@
+package com.SpringBoot.JournalApp.enums;
+
+public enum Sentiment {
+    HAPPY,
+    SAD,
+    ANGRY,
+    ANXIOUS;
+}
